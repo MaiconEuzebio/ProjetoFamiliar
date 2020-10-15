fab641fc4369515c3855e6f6e6b30f3d60a039fc
kaaaa <kaaaa@192.168.25.135>
2020-10-15 13:58:51 -0300

    Adicionado Marca

---
+++ WebContent/js/controllers/marcaController.js
@@ -0,0 +1,198 @@
+app.controller("marcaController", function ($scope, requisicaoService, filterFilter, orderByFilter) {
+		
+	$scope.vizualizarCadastro = false;
+	$scope.mostrarAguarde = false;
+	$scope.tela = "Produto > Marca"	
+	
+	$scope.marcas                = [];
+	$scope.showModalConfirmacao = false;
+	$scope.showModalAviso       = false;
+	$scope.mostrarAguarde       = false;
+	$scope.visualizaCadastro 	= false;
+	$scope.mensagemModal 	 	= '';
+	$scope.mensagemRodape 	 	= '';
+	$scope.mostrarAguarde 		= true;
+	$scope.campoOrdenacao 		= 'descricao';
+	
+	atualizarTela();	
+	
+	/*
+    /////////////////////////////////////////////////////////////////
+	// FUNÇÕES                                                     //
+    /////////////////////////////////////////////////////////////////
+    */
+    $scope.voltar = function(){
+    	$scope.visualizaCadastro 	= false;
+    }
+
+    $scope.btnIncluir = function(){
+    	$scope.mensagemRodape = "";
+    	$scope.mensagemModal  = "";
+    	$scope.mostrarAguarde = true;
+    	
+    	$scope.marca		       = {};
+    	$scope.marca.id       = null;
+    	$scope.marca.descricao   = null;
+    	$scope.marca.status	   = 1;
+    	
+    	$scope.mostrarAguarde    = false;
+    	$scope.visualizaCadastro = true;
+    }
+
+    $scope.btnEditar = function(){
+    	$scope.mensagemRodape = "";
+    	$scope.mensagemModal  = "";
+    	
+    	
+    	if (!$scope.objetoSelecionado) {
+            $scope.mensagemModal   = "É necessário selecionar o registro que deseja editar!";
+        	$('#modalAtencao').modal();
+    		return;
+    	}
+    	var param = {
+			int1: $scope.objetoSelecionado.id
+		}
+    	$scope.mostrarAguarde = true;
+    	//obter a marca
+    	requisicaoService.requisitarPOST("marca/obterPorId", param , function(retorno) {
+			if (!retorno.isValid) {
+    			$scope.mensagemModal  = retorno.msg;
+    			$scope.showModalAviso = true;
+    			$scope.mostrarAguarde = false;
+        		return;
+    		}
+			
+			$scope.marca			   = retorno.data;
+
+	    	$scope.mostrarAguarde    = false;
+	        $scope.visualizaCadastro = true;
+		});
+    }
+
+    $scope.btnExcluir = function(){
+    	$scope.mensagemRodape = "";
+    	$scope.mensagemModal  = "";
+    	if (!$scope.objetoSelecionado) {
+            $scope.mensagemModal  = "É necessário selecionar o registro que deseja excluir!";
+        	$('#modalAtencao').modal();
+    		return;
+    	}
+
+    	$scope.mensagemModal        = 'Deseja realmente excluir o registro?';
+		$('#modalExcluir').modal();
+    }
+
+    $scope.confirmaExcluir = function(){
+    	$scope.mensagemRodape = "";
+    	$scope.mensagemModal  = "";
+    	$scope.mostrarAguarde = true;
+    	
+		var param = {
+			int1: $scope.objetoSelecionado.id
+		}
+
+    	//deletar
+    	requisicaoService.requisitarPOST("marca/removerPorId", param, function(retorno){
+    		if (!retorno.isValid) {
+    			$scope.mensagemModal  = retorno.msg;
+    			$scope.showModalAviso = true;
+    			$scope.mostrarAguarde = false;
+        		return;
+    		}
+    		
+    		$scope.mostrarAguarde       = false;
+    		$scope.showModalConfirmacao = false;
+			$('#modalExcluir').modal('hide');
+    		atualizarTela();
+    	});
+    }
+
+    $scope.retornarPesquisa = function (){
+    	$scope.visualizaCadastro = false;
+    }
+
+    $scope.btnSalvar = function(pmarca){
+    	$scope.mensagemRodape = "";
+    	$scope.mostrarAguarde = true;
+    	
+    	if (!pmarca){
+    		$scope.mensagemRodape = "É necessário o preenchimento do campo Descrição!";
+    		document.getElementById("cDescricao").focus();
+    		$scope.mostrarAguarde = false;
+    		return;
+        }
+    	if (!pmarca.descricao) {
+        	$scope.mensagemRodape = "É necessário o preenchimento do campo Descrição!";
+    		document.getElementById("cDescricao").focus();
+    		$scope.mostrarAguarde = false;
+    		return;
+        }
+
+    	requisicaoService.requisitarPOST("marca/salvar", pmarca, function(retorno){
+    		if (!retorno.isValid) {
+    			$scope.mensagemRodape = retorno.msg;
+    			$scope.mostrarAguarde = false;
+        		return;
+    		}
+    		
+    		$scope.mostrarAguarde    = false;
+    		$scope.visualizaCadastro = false;
+    		atualizarTela();
+    	});
+    }
+
+    /*
+    /////////////////////////////////////////////////////////////////
+	// PAGINAÇÃO E TABELA                                          //
+    /////////////////////////////////////////////////////////////////
+    */
+    
+    function atualizarTela(){
+    	$scope.mensagemRodape = "";
+    	$scope.mensagemModal  = "";
+    	$scope.mostrarAguarde = true;
+    	
+		//obter todos os registros
+    	requisicaoService.requisitarGET("marca/obterTodos", function(retorno) {
+    		if (!retorno.isValid) {
+    			$scope.mensagemModal  = retorno.msg;
+    			$scope.showModalAviso = true;
+    			$scope.mostrarAguarde = false;
+        		return;
+    		}
+			$scope.marcas = retorno.data;
+			
+			$scope.pesquisar();
+			
+			$scope.mostrarAguarde = false;
+		});
+	}
+	
+	$scope.pesquisar = function(){
+		$scope.marcasFiltradas = orderByFilter(filterFilter($scope.marcas,{id:$scope.idFilter,
+													                     descricao: $scope.descricaoFilter}), $scope.campoOrdenacao);
+		
+	}
+	
+	
+    $scope.selecionarLinha = function(objeto) {
+       $scope.objetoSelecionado = objeto;
+    }
+
+	$scope.ordenacao = function (pcampo) {
+		if ($scope.campoOrdenacao == '+'+pcampo || $scope.campoOrdenacao == '-'+pcampo) {
+    		$scope.reverseOrdenacao = !$scope.reverseOrdenacao;
+    	} else {
+    		$scope.reverseOrdenacao = false;
+    	}
+    	
+    	if ($scope.reverseOrdenacao) {
+    		$scope.campoOrdenacao   = '-'+pcampo;	
+    	} else {
+    		$scope.campoOrdenacao   = '+'+pcampo;
+    	}
+    	
+    	$scope.pesquisar();
+    }
+		
+});
---
+++ src/br/com/erp/resources/MarcaImp.java
@@ -0,0 +1,124 @@
+package br.com.erp.resources;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import br.com.erp.json.ParamJson;
+import br.com.erp.model.Marca;
+import br.com.erp.util.UnidadePersistencia;
+
+@Path("marca")
+public class MarcaImp {
+
+	@Path("salvar")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Marca save(Marca marca) {
+
+		EntityManager em = UnidadePersistencia.createEntityManager();
+
+		try {
+			em.getTransaction().begin();
+			if (marca.getId() == null) {
+				em.persist(marca);
+			} else {
+				em.merge(marca);
+			}
+			em.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		} finally {
+			em.close();
+		}
+		return marca;
+	}
+
+	@Path("obterPorId")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Marca findByID(ParamJson paramJson) {
+		EntityManager em = UnidadePersistencia.createEntityManager();
+		Marca marca = null;
+		try {
+			marca = em.find(Marca.class, paramJson.getInt1());
+		} catch (Exception e) {
+			System.err.println(e);
+		} finally {
+			em.close();
+		}
+		return marca;
+	}
+
+	@Path("obterTodos")
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public List<Marca> obterTodos() {
+		EntityManager em = UnidadePersistencia.createEntityManager();
+		List<Marca> marcas = null;
+
+		try {
+			marcas = em.createQuery("select a " 
+							     + "  from Marca a").getResultList();
+
+		} catch (Exception e) {
+
+		} finally {
+			em.close();
+		}
+		return marcas;
+	}
+
+	@Path("obterTodosAtivos")
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public List<Marca> obterTodosAtivos() {
+		EntityManager em = UnidadePersistencia.createEntityManager();
+		List<Marca> marcas = null;
+
+		try {
+			marcas = em.createQuery("select a " 
+								 + "  from Marca a" 
+								 + " where a.status = 1").getResultList();
+
+		} catch (Exception e) {
+
+		} finally {
+			em.close();
+		}
+		return marcas;
+	}
+
+	@Path("removerPorId")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public void remove(ParamJson paramJson) {
+		EntityManager em = UnidadePersistencia.createEntityManager();
+
+		try {
+			Marca marca = em.find(Marca.class, paramJson.getInt1());
+			em.getTransaction().begin();
+			em.remove(marca);
+			em.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+
+		} finally {
+			em.close();
+		}
+
+	}
+}

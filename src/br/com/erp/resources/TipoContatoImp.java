ca393a6f9be83cf7a67980b3894ddac9ee41278c
maicon <maicon@192.168.25.114>
2020-10-15 13:54:07 -0300

    Adicionado tipo de contato

---
+++ src/br/com/erp/resources/TipoContatoImp.java
@@ -0,0 +1,126 @@
+package br.com.erp.resources;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import br.com.erp.json.ParamJson;
+import br.com.erp.model.TipoContato;
+import br.com.erp.util.UnidadePersistencia;
+
+@Path("tipoContato")
+public class TipoContatoImp {
+	
+	@Path("salvar")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public TipoContato save(TipoContato tipoContato) {
+
+		EntityManager em = UnidadePersistencia.createEntityManager();
+
+		try {
+			em.getTransaction().begin();
+			if (tipoContato.getId() == null) {
+				em.persist(tipoContato);
+			} else {
+				em.merge(tipoContato);
+			}
+			em.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		} finally {
+			em.close();
+		}
+		return tipoContato;
+	}
+	
+	@Path("obterPorId")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public TipoContato findByID(ParamJson paramJson) {
+		EntityManager em = UnidadePersistencia.createEntityManager();
+		TipoContato tipoContato = null;
+		try {
+			tipoContato = em.find(TipoContato.class, paramJson.getInt1());
+		} catch (Exception e) {
+			System.err.println(e);
+		} finally {
+			em.close();
+		}
+		return tipoContato;
+	}
+	
+	@Path("obterTodos")
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public List<TipoContato> obterTodos() {
+		EntityManager em = UnidadePersistencia.createEntityManager();
+		List<TipoContato> tipoContatos = null;
+
+		try {
+			tipoContatos = em.createQuery("select a " 
+							     + "  from TipoContato a").getResultList();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			em.close();
+		}
+		return tipoContatos;
+	}
+	
+	@Path("obterTodosAtivos")
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public List<TipoContato> obterTodosAtivos() {
+		EntityManager em = UnidadePersistencia.createEntityManager();
+		List<TipoContato> tipoContatos = null;
+
+		try {
+			tipoContatos = em.createQuery("select a " 
+								 + "  from TipoContato a" 
+								 + " where a.status = 1").getResultList();
+
+		} catch (Exception e) {
+
+		} finally {
+			em.close();
+		}
+		return tipoContatos;
+	}
+	
+	@Path("removerPorId")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public void remove(ParamJson paramJson) {
+		EntityManager em = UnidadePersistencia.createEntityManager();
+
+		try {
+			TipoContato tipoContato = em.find(TipoContato.class, paramJson.getInt1());
+			em.getTransaction().begin();
+			em.remove(tipoContato);
+			em.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+
+		} finally {
+			em.close();
+		}
+
+	}
+	
+}
